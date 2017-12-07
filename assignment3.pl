416cc7049ef692ec204dc8ae612e4615a0bf51ce
Velajuel121 <32167792+Velajuel121@users.noreply.github.com>
2017-12-07 14:23:23 +0100

    Update assignment3.pl

--- assignment3.pl
+++ assignment3.pl
@@ -11,9 +11,6 @@ addElement(X,[ ],[X]).
 addElement(X,[H|T],[H|Z]) :- addElement(X,T,Z).
 hasLength([],0).
 hasLength([_|X],L) :- hasLength(X,N),L is N+1 .
-/*del(_,[],[]).
-del(X, [X|L1],L2) :- del(X,L1,L2).
-del(X, [H|L1],[H|L2]) :- X\==H,del(X,L1,L2).
-*/
+
 remove([Head|Tail], Head, Tail).
 remove([Head|Tail], E, [Head|NewTail]):- remove(Tail, E, NewTail).
